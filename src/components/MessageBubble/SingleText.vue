887009d55554c7865d1aa2f716dbe5ecc1b41204
lstypka <l.stypka@emporix.com>
2024-03-03 12:29:48 +0100

    Added markdown to html renderer

--- src/components/MessageBubble/SingleText.vue
+++ src/components/MessageBubble/SingleText.vue
@@ -1,13 +1,26 @@
 <template lang="pug">
 .qkb-msg-bubble-component.qkb-msg-bubble-component--single-text
-  .qkb-msg-bubble-component__text <div v-html="mainData.text"></div>
+  .qkb-msg-bubble-component__text <div v-html="convertToHtml()"></div>
 </template>
 <script>
+const showdown = require('showdown');
+const converter = new showdown.Converter();
+
+
 export default {
+  components: {
+  },
   props: {
     mainData: {
       type: Object
     }
+  },
+  methods: {
+    convertToHtml() {
+      return converter.makeHtml(this.mainData.text)
+    }
+
   }
+
 }
 </script>
